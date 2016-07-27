4a5cc3c169ce550a0fc1322a7c3f6e3a9e3dad6c
Viacheslav Dreval <drevalv1991@gmail.com>
2016-07-28 00:11:53 +0300

    add forms to systemjs

--- dev/boot.ts
+++ dev/boot.ts
@@ -1,5 +1,8 @@
 /// <reference path="../typings/index.d.ts" />
 import {AppComponent} from "./app.component";
 import {bootstrap} from "@angular/platform-browser-dynamic";
+import {disableDeprecatedForms, provideForms} from '@angular/forms';
 
-bootstrap(AppComponent);
+bootstrap(AppComponent, [disableDeprecatedForms(), provideForms()]);
+
+//todo change methods in bootstrap as in the end of https://docs.google.com/document/d/1RIezQqE4aEhBRmArIAS1mRIZtWFf6JxN_7B4meyWK0Y/pub
