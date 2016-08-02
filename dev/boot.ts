ccfc8d2865ab07c698ba5c498b478c4dd9a4fb57
Viacheslav Dreval <drevalv1991@gmail.com>
2016-08-02 21:51:37 +0300

    provide http

--- dev/boot.ts
+++ dev/boot.ts
@@ -2,7 +2,8 @@
 import {AppComponent} from "./app.component";
 import {bootstrap} from "@angular/platform-browser-dynamic";
 import {disableDeprecatedForms, provideForms} from '@angular/forms';
+import {HTTP_PROVIDERS} from "@angular/http";
 
-bootstrap(AppComponent, [disableDeprecatedForms(), provideForms()]);
+bootstrap(AppComponent, [disableDeprecatedForms(), provideForms(), HTTP_PROVIDERS]);
 
 //todo change methods in bootstrap as in the end of https://docs.google.com/document/d/1RIezQqE4aEhBRmArIAS1mRIZtWFf6JxN_7B4meyWK0Y/pub
